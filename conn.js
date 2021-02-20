a36f0246efdee9219953738d958711225f405bf6
yssacst <38430578+yssacst@users.noreply.github.com>
2021-02-20 03:10:42 -0300

    Test BD online

--- conn.js
+++ conn.js
@@ -1,9 +1,14 @@
 const mysql = require('mysql')
 const conn = mysql.createConnection({
-    host : 'localhost',
+    /*host : 'localhost',
     user : 'root',
     password : '',
-    database : 'ecorenda2'
+    database : 'ecorenda2'*/
+    
+    host : '162.241.203.62',
+    user : 'gustav31_adm_ecorenda',
+    password : 'ecorenda2020',
+    database : 'gustav31_ecorenda2'
 })
 
 module.exports = conn
