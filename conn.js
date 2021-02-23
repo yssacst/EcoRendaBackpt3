6d35d1cb2e52446afc59f81285d5a8d72656a70b
yssacst <38430578+yssacst@users.noreply.github.com>
2021-02-23 08:19:09 -0300

    clever

--- conn.js
+++ conn.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql')
 const conn = mysql.createConnection({
-    host : 'localhost',
+   /* host : 'localhost',
     user : 'root',
     password : '',
-    database : 'ecorenda2'
+    database : 'ecorenda2'*/
     /*host : '162.241.203.62',
     user : 'gustav31_adm_ecorenda',
-    password : 'ecorenda2020',
+    password : '',
     database : 'gustav31_ecorenda2'*/
+    host : 'bmqafcjabntoriozlsak-mysql.services.clever-cloud.com',
+    user : 'u4hj65p0ncmscjnv',
+    password : 'QLcmd14mDVsrTWTGGfIc',
+    database : 'bmqafcjabntoriozlsak'
+    
+    
 })
 
 module.exports = conn
